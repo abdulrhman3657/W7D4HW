e2c9274bbdc7ba55fe444ed124b81cc076db6bd4
user1 <user01@em/com>
2025-05-28 21:12:12 +0300

    final v6

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,14 @@
 import { Link } from "react-router";
+import { useState } from "react";
 
 function Navbar() {
+  const [mbileMenue, setMobileMenue] = useState(false);
+
+  console.log(mbileMenue);
+
   return (
     <div className="">
-      <div className="p-3 bg-green-900 text-white flex justify-around items-center">
+      <div className="p-3 bg-green-900 text-white flex flex-col lg:flex-row gap-3 lg:gap-0 justify-between lg:justify-around items-center">
         <img className="w-10  rounded-xl" src="react.svg" alt="" />
         <ul className="hidden lg:flex gap-5">
           <li className="hover:text-black hover:bg-white p-2 rounded-xl">
@@ -13,7 +18,18 @@ function Navbar() {
             <Link to={"login"}>Login</Link>
           </li>
           <li className="hover:text-black hover:bg-white p-2 rounded-xl">
-            <Link to={"signup"}>Signup</Link>
+            <Link to={"signup"}>Sign up</Link>
+          </li>
+        </ul>
+        <ul className="lg:hidden text-center">
+          <li className="hover:text-black hover:bg-white p-2 rounded-xl">
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li className="hover:text-black hover:bg-white p-2 rounded-xl">
+            <Link to={"login"}>Login</Link>
+          </li>
+          <li className="hover:text-black hover:bg-white p-2 rounded-xl">
+            <Link to={"signup"}>Sign up</Link>
           </li>
         </ul>
       </div>
